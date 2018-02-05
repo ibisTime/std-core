695472e9825822c51e1c3501b7563ceb91fae029
haiqingzheng <leo.zheng@hichengdai.com>
2018-02-05 11:10:29 +0800

    倍可盈v210第一次发包

--- src/main/java/com/cdkj/core/APIServlet.java
+++ src/main/java/com/cdkj/core/APIServlet.java
@@ -43,8 +43,8 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:esmm_1.0.0 \n");
-        writer.append("Description:6th \n");
+        writer.append("Version:2.1.0 \n");
+        writer.append("Description:coin 1st \n");
         writer.flush();
     }
 
