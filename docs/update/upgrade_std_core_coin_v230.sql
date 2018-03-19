308e00fdb75c9f06f7fafef53ee689e85f9fba5b
haiqingzheng <leo.zheng@hichengdai.com>
2018-03-19 22:31:32 +0800

    v230今日收尾

--- docs/update/upgrade_std_core_coin_v230.sql
+++ docs/update/upgrade_std_core_coin_v230.sql
@@ -7,3 +7,5 @@ INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_d
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','coin_status','0','待发布','admin',now(),'','CD-COIN000017','CD-COIN000017');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','coin_status','1','已发布','admin',now(),'','CD-COIN000017','CD-COIN000017');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','coin_status','2','已撤下','admin',now(),'','CD-COIN000017','CD-COIN000017');
+
+INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','channel_type','1','线上','admin',now(),'','CD-COIN000017','CD-COIN000017');
