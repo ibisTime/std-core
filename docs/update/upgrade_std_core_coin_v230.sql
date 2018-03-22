978fece1ad1b3dab504ad3b9fb3883786634f5fa
haiqingzheng <leo.zheng@hichengdai.com>
2018-03-22 15:59:02 +0800

    脚本整理

--- docs/update/upgrade_std_core_coin_v230.sql
+++ docs/update/upgrade_std_core_coin_v230.sql
@@ -1,4 +1,12 @@
 
+DELETE FROM `tsys_config` WHERE `type`='collection_rule_eth';
+DELETE FROM `tsys_config` WHERE `id`='withdraw_rule_eth';
+DELETE FROM `tsys_config` WHERE `type`='collection_rule_sc';
+DELETE FROM `tsys_config` WHERE `id`='withdraw_rule_sc';
+DELETE FROM `tsys_config` WHERE `type`='collection_rule_btc';
+DELETE FROM `tsys_config` WHERE `id`='withdraw_rule_btc';
+
+
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('0',NULL,'coin_type','币种类型','admin',now(),'','CD-COIN000017','CD-COIN000017');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','coin_type','0','原生币','admin',now(),'','CD-COIN000017','CD-COIN000017');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','coin_type','1','token币','admin',now(),'','CD-COIN000017','CD-COIN000017');
