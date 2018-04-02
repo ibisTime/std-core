0073b3947ae30bcffb8d3766309b50a75676c406
haiqingzheng <leo.zheng@hichengdai.com>
2018-04-02 20:23:57 +0800

    tokenv120第二次发包

--- src/main/java/com/cdkj/core/APIServlet.java
+++ src/main/java/com/cdkj/core/APIServlet.java
@@ -44,7 +44,7 @@ public class APIServlet extends HttpServlet {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
         writer.append("Version:1.2.0 \n");
-        writer.append("Description:std-core token 1st \n");
+        writer.append("Description:std-core token 2nd \n");
         writer.flush();
     }
 
