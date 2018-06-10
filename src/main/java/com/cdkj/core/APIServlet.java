fd9b1035806faab045ecce36807e86c9824c9238
haiqingzheng <leo.zheng@hichengdai.com>
2018-06-10 19:54:34 +0800

    版本信息修改

--- src/main/java/com/cdkj/core/APIServlet.java
+++ src/main/java/com/cdkj/core/APIServlet.java
@@ -43,8 +43,8 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.2.0 \n");
-        writer.append("Description:std-core token 3rd \n");
+        writer.append("Version:1.3.0 \n");
+        writer.append("Description:std-core THA 1st \n");
         writer.flush();
     }
 
