3b44f6e6919c5dcd55e4f92699707ac4c6c13958
haiqingzheng <leo.zheng@hichengdai.com>
2018-06-28 19:17:59 +0800

    新建分支，版本信息修改

--- src/main/java/com/cdkj/core/APIServlet.java
+++ src/main/java/com/cdkj/core/APIServlet.java
@@ -43,7 +43,7 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.3.0 \n");
+        writer.append("Version:1.4.0 \n");
         writer.append("Description:std-core THA 1st \n");
         writer.flush();
     }
