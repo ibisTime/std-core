4d9f13a92465f3f0aa0211ea6cd47f9cfd3658a9
haiqingzheng <leo.zheng@hichengdai.com>
2018-06-30 00:08:18 +0800

    脚本新增

--- docs/update/upgrade_std_core_coin_v140.sql
+++ docs/update/upgrade_std_core_coin_v140.sql
@@ -11,6 +11,9 @@ CREATE TABLE `tstd_country` (
   PRIMARY KEY (`inter_code`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+UPDATE `dev_tha_std_core`.`tsys_dict` SET `dvalue`='基于ETH的token币' WHERE `parent_key`='coin_type' AND `dkey`='1';
+INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','coin_type','2','基于WAN的token币','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
+
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('0',NULL,'red_packet_status','红包状态','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','red_packet_status','0','已发布','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','red_packet_status','1','未抢完','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
@@ -26,5 +29,4 @@ INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_d
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','jour_biz_type_user','sendredpacket_out','发红包','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
 INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','jour_biz_type_user','redpacket_back','红包退回','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
 
-INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','jour_biz_type_plat','hb_in','红包转入','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_dict` (`type`,`parent_key`,`dkey`,`dvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('1','jour_biz_type_plat','hb_out','红包转出','admin',now(),'','CD-TOKEN00018','CD-TOKEN00018');
+
