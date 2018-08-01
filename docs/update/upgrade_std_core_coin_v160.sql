6b43c021e1c66e89c95d56794592785b03803986
haiqingzheng <leo.zheng@hichengdai.com>
2018-08-01 21:28:12 +0800

    update

--- docs/update/upgrade_std_core_coin_v160.sql
+++ docs/update/upgrade_std_core_coin_v160.sql
@@ -1,38 +1,39 @@
 
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','questions_ZH_CN','常见问题中文版','admin',now(),'常见问题','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','reg_protocol_ZH_CN','注册协议中文版','admin',now(),'注册协议','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','privacy_ZH_CN','隐私政策中文版','admin',now(),'隐私政策','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_ZH_CN','什么是助记词中文版','admin',now(),'什么是助记词','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','create_wallet_ZH_CN','创建钱包流程中文版','admin',now(),'创建钱包流程','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_backup_ZH_CN','如何备份助记词中文版','admin',now(),'如何备份助记词','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','red_packet_rule_ZH_CN','红包规则中文版','admin',now(),'红包规则','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','global_master_ZH_CN','首创玩法中文版','admin',now(),'首创玩法','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','quantitative_finance_ZH_CN','量化理财中文版','admin',now(),'量化理财','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','yubibao_ZH_CN','余币宝中文版','admin',now(),'余币宝','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','questions_ZH_CN','常见问题中文版','admin',now(),'常见问题（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','reg_protocol_ZH_CN','注册协议中文版','admin',now(),'注册协议（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','privacy_ZH_CN','隐私政策中文版','admin',now(),'隐私政策（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_ZH_CN','什么是助记词中文版','admin',now(),'什么是助记词（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','create_wallet_ZH_CN','创建钱包流程中文版','admin',now(),'创建钱包流程（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_backup_ZH_CN','如何备份助记词中文版','admin',now(),'如何备份助记词（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','red_packet_rule_ZH_CN','红包规则中文版','admin',now(),'红包规则（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','global_master_ZH_CN','首创玩法中文版','admin',now(),'首创玩法（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','quantitative_finance_ZH_CN','量化理财中文版','admin',now(),'量化理财（中文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','yubibao_ZH_CN','余币宝中文版','admin',now(),'余币宝（中文）','CD-TOKEN00018','CD-TOKEN00018');
 
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','questions_EN','常见问题英文版','admin',now(),'常见问题','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','reg_protocol_EN','注册协议英文版','admin',now(),'注册协议','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','privacy_EN','隐私政策英文版','admin',now(),'隐私政策','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_EN','什么是助记词中文版','admin',now(),'什么是助记词','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','create_wallet_EN','创建钱包流程中文版','admin',now(),'创建钱包流程','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_backup_EN','如何备份助记词中文版','admin',now(),'如何备份助记词','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','red_packet_rule_EN','红包规则中文版','admin',now(),'红包规则','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','global_master_EN','首创玩法中文版','admin',now(),'首创玩法','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','quantitative_finance_EN','量化理财中文版','admin',now(),'量化理财','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','yubibao_EN','余币宝中文版','admin',now(),'余币宝','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','questions_EN','常见问题英文版','admin',now(),'常见问题（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','reg_protocol_EN','注册协议英文版','admin',now(),'注册协议（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','privacy_EN','隐私政策英文版','admin',now(),'隐私政策（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_EN','什么是助记词英文版','admin',now(),'什么是助记词（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','create_wallet_EN','创建钱包流程英文版','admin',now(),'创建钱包流程（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_backup_EN','如何备份助记词英文版','admin',now(),'如何备份助记词（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','red_packet_rule_EN','红包规则英文版','admin',now(),'红包规则（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','global_master_EN','首创玩法英文版','admin',now(),'首创玩法（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','quantitative_finance_EN','量化理财英文版','admin',now(),'量化理财（英文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','yubibao_EN','余币宝英文版','admin',now(),'余币宝（英文）','CD-TOKEN00018','CD-TOKEN00018');
 
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','questions_KO','常见问题韩文版','admin',now(),'常见问题','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','reg_protocol_KO','注册协议韩文版','admin',now(),'注册协议','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','privacy_KO','隐私政策韩文版','admin',now(),'隐私政策','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_KO','什么是助记词中文版','admin',now(),'什么是助记词','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','create_wallet_KO','创建钱包流程中文版','admin',now(),'创建钱包流程','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_backup_KO','如何备份助记词中文版','admin',now(),'如何备份助记词','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','red_packet_rule_KO','红包规则中文版','admin',now(),'红包规则','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','global_master_KO','首创玩法中文版','admin',now(),'首创玩法','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','quantitative_finance_KO','量化理财中文版','admin',now(),'量化理财','CD-TOKEN00018','CD-TOKEN00018');
-INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','yubibao_KO','余币宝中文版','admin',now(),'余币宝','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','questions_KO','常见问题韩文版','admin',now(),'常见问题（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','reg_protocol_KO','注册协议韩文版','admin',now(),'注册协议（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','privacy_KO','隐私政策韩文版','admin',now(),'隐私政策（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_KO','什么是助记词韩文版','admin',now(),'什么是助记词（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','create_wallet_KO','创建钱包流程韩文版','admin',now(),'创建钱包流程（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','mnemonic_backup_KO','如何备份助记词韩文版','admin',now(),'如何备份助记词（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','red_packet_rule_KO','红包规则韩文版','admin',now(),'红包规则（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','global_master_KO','首创玩法韩文版','admin',now(),'首创玩法（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','quantitative_finance_KO','量化理财韩文版','admin',now(),'量化理财（韩文）','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('sys_txt','yubibao_KO','余币宝韩文版','admin',now(),'余币宝（韩文）','CD-TOKEN00018','CD-TOKEN00018');
 
-
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('hb','red_packet_num_max','10000','admin',now(),'最大红包个数','CD-TOKEN00018','CD-TOKEN00018');
+INSERT INTO `tsys_config` (`type`,`ckey`,`cvalue`,`updater`,`update_datetime`,`remark`,`company_code`,`system_code`) VALUES ('hb','red_packet_amount_max','1000000','admin',now(),'最大红包总金额','CD-TOKEN00018','CD-TOKEN00018');
 
 
 
