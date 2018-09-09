3424dc92a88b1d989e2cd6c7f87b3c136af9e294
pyez1158 <pyez1158@163.com>
2018-09-09 14:11:58 +0800

    封板提交

--- src/main/java/com/cdkj/core/APIServlet.java
+++ src/main/java/com/cdkj/core/APIServlet.java
@@ -41,7 +41,7 @@ public class APIServlet extends HttpServlet {
             HttpServletResponse response) throws ServletException, IOException {
         logger.info("Successful Deployment");
         PrintWriter writer = response.getWriter();
-        writer.append("Version:1.2.0 \n");
+        writer.append("Version:1.3.0 \n");
         writer.append("Description:1st \n");
 
         writer.append("Version:1.1.0 \n");
